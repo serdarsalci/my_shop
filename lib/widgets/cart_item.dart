a8c11ca399fcb6d382910d90043ddd2a3fb5eb36
Serdar Salci <serdarsalci@gmail.com>
2021-06-19 21:59:47 -0400

    part 210 -- Making cart items dissmissible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
   CartItem(
     this.id,
+    this.productId,
     this.price,
     this.quantity,
     this.title,
@@ -15,23 +19,43 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        print(direction);
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text("Delete", style: TextStyle(color: Colors.white, fontSize: 25)),
+            Icon(Icons.delete, color: Colors.white, size: 30),
+          ],
+        ),
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Padding(
-                padding: EdgeInsets.all(4),
-                child: FittedBox(
-                  child: Text('\$$price'),
+        padding: EdgeInsets.symmetric(horizontal: 15),
+      ),
+      child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Padding(
+                  padding: EdgeInsets.all(4),
+                  child: FittedBox(
+                    child: Text('\$$price'),
+                  ),
                 ),
               ),
+              title: Text(title),
+              subtitle: Text('Total: \$${price * quantity}'),
+              trailing: Text('$quantity x'),
             ),
-            title: Text(title),
-            subtitle: Text('Total: \$${price * quantity}'),
-            trailing: Text('$quantity x'),
-          ),
-        ));
+          )),
+    );
   }
 }
