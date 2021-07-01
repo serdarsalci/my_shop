bc188a9bd5efcc8a91e633120c1bb9d21682286e
Serdar Salci <serdarsalci@gmail.com>
2021-07-01 01:03:05 -0400

    part 255 --Updating Data via PATCH Requests

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -71,18 +71,13 @@ class _EditProductScreenState extends State<EditProductScreen> {
     if (!_isValid) {
       return;
     }
-
     setState(() {
       _isLoading = true;
     });
     _form.currentState.save();
     if (_edittedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_edittedProduct.id, _edittedProduct);
-      setState(() {
-        _isLoading = false;
-      });
-      Navigator.of(context).pop();
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
@@ -102,13 +97,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
       }
+      // finally {
+      //   setState(() {
+      //     _isLoading = false;
+      //   });
+      //   Navigator.of(context).pop();
+      // }
     }
+    setState(() {
+      _isLoading = false;
+    });
+    Navigator.of(context).pop();
   }
 
   @override
